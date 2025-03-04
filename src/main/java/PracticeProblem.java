0c2349bb27a256140467a558e1d982601b40c9ca
cindy-pan-ics4u1-02-25 <cindy.pan@student.tdsb.on.ca>
2025-03-04 03:18:01 +0000

    Update

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,27 +1,29 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
-
+	public static int sum2D(int[][] array2D){
+		int sum =0;
+		for (int row = 0; row<array2D.length; row++){
+			for (int col =0; col<array2D[row].length; col++){
+				sum = sum+array2D[row][col];
+			}
+		}
+		return sum;
 	}
-
-	public static void q1() {
-		//Write question 1 code here
+	
+	public static int sumRow(int[][] arrTwo, int rowNum){
+		int sum = 0;
+		for (int x =0; x<arrTwo[rowNum].length; x++){
+			sum = sum+arrTwo[rowNum][x];
+		}
+		return sum;
 	}
-
-	public static void q2() {
-		//Write question 2 code here
-	}
-
-	public static void q3() {
-		//Write question 3 code here
-	}
-
-	public static void q4() {
-		//Write question 4 code here
-	}
-
-	public static void q5() {
-		//Write question 5 code here
+	public static int sumColumn(int[][] arr2D, int colNum){
+		// row = y; col =x
+		int sum=0;
+		for (int y=0; y<arr2D.length; y++){
+			sum = sum + arr2D[y][colNum];
+		}
+		return sum;
 	}
 
 }
